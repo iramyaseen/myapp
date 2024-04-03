bf4874cf22e297a724ec6c20e6a6ef16669879cd
iramyaseen <75573544+iramyaseen@users.noreply.github.com>
2024-04-03 17:39:57 +0500

    pagination api added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Pagination, PaginationItem, PaginationLink, Table } from "reactstrap";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async (offset) => {
+    try {
+      const response = await fetch(
+        `https://api.escuelajs.co/api/v1/products?offset=0&limit=${offset}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(5);
+  }, []);
+
+  const handlePageChange = (pageNumber) => {
+    const offset = pageNumber * 5;
+    fetchProducts(offset);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "30px" }}>
+      <Pagination>
+        {[1, 2, 3, 4, 5].map((pageNumber) => (
+          <PaginationItem key={pageNumber}>
+            <PaginationLink onClick={() => handlePageChange(pageNumber)}>
+              {pageNumber}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+      </Pagination>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>{product.description.slice(0, 40)}</td>
+              <td>
+                <img src={product.category?.image} alt="" width={100} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
